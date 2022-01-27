bc8913ffc158d429eec60d949333995309c006af
KuleshN <kulesh.nick@gmail.com>
2022-01-27 20:35:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict"
+alert ("Hello");
+
+const numberOfFilms = +prompt("Сколько фильмов ты смотрел?");
+alert (numberOfFilms);
+
+const personalMovieDB ={
+count = numberOfFilms,
+movies:{},
+actors:{},
+genres:[],
+privat: false
+};
+console.log(personalMovieDB);
