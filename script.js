e8d34029eacfae1296a44e48defb914500227d26
KuleshN <kulesh.nick@gmail.com>
2022-01-27 21:01:16 +0300

    third commit questions in js file and WTF in HTML

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-"use strict"
+
 alert ("Hello");
 
 const numberOfFilms = +prompt("Сколько фильмов ты смотрел?");
 alert (numberOfFilms);
 
-const personalMovieDB ={
-count = numberOfFilms,
+const personalMovieDB = {
+count: numberOfFilms,
 movies:{},
 actors:{},
 genres:[],
 privat: false
 };
 console.log(personalMovieDB);
+
+const a = prompt("Последний фильм который ты смотрел?"," "),
+      b = +prompt("Насколько оценишь?"," "),
+      c = prompt("Последний фильм который ты смотрел?"," "),
+      d = +prompt("Насколько оценишь?"," ");
+
+      personalMovieDB.movies[a]=b;
+      personalMovieDB.movies[c]=d;
+
+      console.log(personalMovieDB);
