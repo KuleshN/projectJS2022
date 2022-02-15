79f189ddc329ddb88b55b2a1119d6df2c5a39b3f
KuleshN <kulesh.nick@gmail.com>
2022-02-15 11:50:16 +0300

    15.02.2022 added FOR practice2

--- script.js
+++ script.js
@@ -13,12 +13,37 @@ privat: false
 };
 console.log(personalMovieDB);
 
-const a = prompt("Последний фильм который ты смотрел?"," "),
-      b = +prompt("Насколько оценишь?"," "),
-      c = prompt("Последний фильм который ты смотрел?"," "),
-      d = +prompt("Насколько оценишь?"," ");
+// const a = prompt("Последний фильм который ты смотрел?"," "),
+//       b = +prompt("Насколько оценишь?"," "),
+//       c = prompt("Последний фильм который ты смотрел?"," "),
+//       d = +prompt("Насколько оценишь?"," ");
 
-      personalMovieDB.movies[a]=b;
-      personalMovieDB.movies[c]=d;
+//       personalMovieDB.movies[a]=b;
+//       personalMovieDB.movies[c]=d;
+
+if(personalMovieDB.count<10){
+    alert("Просмотрено мало фильмов")
+} else if (personalMovieDB.count>=10&&personalMovieDB.count<=30 ){
+    alert("Вы классический зритель")
+} else if(personalMovieDB.count>30){
+    alert("Вы киноман")
+} else {
+    alert("Произошла ошибка")
+}
+
+      for (let i=0; i <2; i++){
+        const a = prompt("Последний фильм который ты смотрел?"," "),
+        b = +prompt("Насколько оценишь?"," ");
+        personalMovieDB.movies[a]=b;
+         if (a!=null && b!=null && a!="" && b!=""&& a.length<50) {
+            personalMovieDB.movies[a]=b;
+            console.log('done');
+            } else {
+            alert("Заполни снова");
+            i--;    
+             }
+      }
+
+ 
 
       console.log(personalMovieDB);
